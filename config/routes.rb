52aaefe6d80efd2790224b45b5a531ad7a98995b
Mithu23 <mithu.selvakumar@utoronto.ca>
2015-11-10 23:19:55 -0500

    Add Comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+  resources :comments
+
   devise_for :users
   resources :links do
-  	member do
-  		put "like", to: "links#upvote"
-  		put "dislike", to: "links#downvote"
-  	end
+    member do
+      put "like", to:    "links#upvote"
+      put "dislike", to: "links#downvote"
+    end
+    resources :comments
   end
-
-  root 'links#index'
-
+  root "links#index"
 
 end
